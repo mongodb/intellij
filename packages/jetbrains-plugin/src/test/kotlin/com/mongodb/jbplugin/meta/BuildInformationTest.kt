8816687560fa7ca62b84c1ea15a08a63d8c04518
Kevin Mas Ruiz <kevin.mas@hey.com>
2025-02-06 20:40:51 +0100

    chore: fix test

--- packages/jetbrains-plugin/src/test/kotlin/com/mongodb/jbplugin/meta/BuildInformationTest.kt
+++ packages/jetbrains-plugin/src/test/kotlin/com/mongodb/jbplugin/meta/BuildInformationTest.kt
@@ -1,13 +1,12 @@
 package com.mongodb.jbplugin.meta
 
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 
 class BuildInformationTest {
     @Test
     fun `loads all build information from the resource file`() {
-        assertNotNull(BuildInformation.pluginVersion)
-        assertEquals("<none>", BuildInformation.segmentApiKey)
+        assertEquals("9.21.22", BuildInformation.pluginVersion)
+        assertEquals("SOME_KEY", BuildInformation.segmentApiKey)
     }
 }
