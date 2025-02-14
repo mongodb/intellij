4a4debeac14056876f97b42f830fbd41724a9a16
Kevin Mas Ruiz <kevin.mas@hey.com>
2025-02-14 13:16:33 +0100

    chore(ci): ensure spec changes when the mql model or engines change INTELLIJ-228 (#152)

--- buildSrc/src/main/kotlin/com.mongodb.intellij.isolated-module.gradle.kts
+++ buildSrc/src/main/kotlin/com.mongodb.intellij.isolated-module.gradle.kts
@@ -4,6 +4,7 @@ import org.gradle.kotlin.dsl.support.delegates.TaskContainerDelegate.*
 import org.jlleitschuh.gradle.ktlint.KtlintExtension
 import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.io.ByteArrayOutputStream
 
 plugins {
     id("java")
@@ -96,3 +97,43 @@ configure<KtlintExtension> {
     }
 }
 
+tasks.register("checkSpecUpdates") {
+    group = "verification"
+    description = "Fails if a Kotlin file changed but no specification file was updated"
+
+    doLast {
+        val rootDir = "${rootDir.absolutePath}"
+        val baseDir = "${project.projectDir.path}"
+        val specsDir = "$baseDir/src/docs/"
+
+        if (!File(specsDir).exists()) {
+            logger.lifecycle("Skipping checkSpecUpdates: $specsDir does not exist.")
+            return@doLast
+        } else {
+            logger.lifecycle("Verifying specifications.")
+        }
+
+        val outputStream = ByteArrayOutputStream()
+        exec {
+            workingDir = project.rootDir
+            standardOutput = outputStream
+            commandLine("git", "diff", "--name-only", "origin/main")
+        }
+
+        val changedFiles = outputStream.toString().trim().lines().map { "$rootDir/$it" }
+
+        logger.quiet("List of changed files:")
+        changedFiles.forEach { file ->
+            logger.quiet(file)
+        }
+
+        val codeChanged = changedFiles.any { it.startsWith("$baseDir/src/main/kotlin/") && it.endsWith(".kt") }
+        val specChanged = changedFiles.any { it.startsWith(specsDir) }
+
+        if (codeChanged && !specChanged) {
+            logger.error("The specification is not up to date with the latest code changes.")
+            logger.error("Please update the relevant files in $specsDir or add the 'skip-spec-check' label to the PR.")
+            throw GradleException()
+        }
+    }
+}
