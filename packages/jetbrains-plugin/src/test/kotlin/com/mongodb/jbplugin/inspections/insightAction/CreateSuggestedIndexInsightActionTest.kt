8701f6d6f5c48315dd1eabca788e399097ae46ef
Kevin Mas Ruiz <kevin.mas@hey.com>
2025-03-27 18:40:50 +0100

    chore: open editor under test

--- packages/jetbrains-plugin/src/test/kotlin/com/mongodb/jbplugin/inspections/insightAction/CreateSuggestedIndexInsightActionTest.kt
+++ packages/jetbrains-plugin/src/test/kotlin/com/mongodb/jbplugin/inspections/insightAction/CreateSuggestedIndexInsightActionTest.kt
@@ -1,22 +1,29 @@
 package com.mongodb.jbplugin.inspections.insightAction
 
 import com.intellij.database.dataSource.LocalDataSource
+import com.intellij.database.dataSource.localDataSource
+import com.intellij.database.psi.DbDataSource
+import com.intellij.database.psi.DbPsiFacade
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
+import com.mongodb.jbplugin.editor.CachedQueryService
 import com.mongodb.jbplugin.editor.DatagripConsoleEditor
-import com.mongodb.jbplugin.editor.dataSource
 import com.mongodb.jbplugin.fixtures.IntegrationTest
 import com.mongodb.jbplugin.fixtures.mockDataSource
+import com.mongodb.jbplugin.fixtures.withMockedService
 import com.mongodb.jbplugin.linting.QueryInsight
-import com.mongodb.jbplugin.meta.service
 import com.mongodb.jbplugin.mql.Node
 import com.mongodb.jbplugin.observability.probe.CreateIndexIntentionProbe
+import com.mongodb.jbplugin.ui.viewModel.ConnectionState
 import com.mongodb.jbplugin.ui.viewModel.ConnectionStateViewModel
+import com.mongodb.jbplugin.ui.viewModel.SelectedConnectionState
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.runTest
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.mockito.kotlin.any
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
@@ -35,27 +42,75 @@ class CreateSuggestedIndexInsightActionTest {
         val consoleEditor = mock<DatagripConsoleEditor>()
 
         val insightAction = CreateSuggestedIndexInsightAction(probe, consoleEditor)
-        val query = aQuery(project)
+        val query = project.aQuery()
 
         insightAction.apply(QueryInsight.notUsingIndex(query))
 
         verify(probe).intentionClicked(query)
     }
 
-    private fun aQuery(project: Project): Node<PsiElement> {
+    @Test
+    fun `should open a console with the index script`(
+        project: Project,
+        coroutineScope: TestScope,
+    ) = coroutineScope.runTest {
+        val dataSource = mockDataSource()
+        project.connectedTo(dataSource)
+
+        val probe = mock<CreateIndexIntentionProbe>()
+        val consoleEditor = mock<DatagripConsoleEditor>()
+        val cachedQueryService = mock<CachedQueryService>()
+        whenever(cachedQueryService.allSiblingsOf(any())).thenReturn(emptyArray())
+
+        project.withMockedService(cachedQueryService)
+
+        val insightAction = CreateSuggestedIndexInsightAction(probe, consoleEditor)
+        val query = project.aQuery()
+
+        insightAction.apply(QueryInsight.notUsingIndex(query))
+
+        verify(consoleEditor).openConsoleForDataSource(project, dataSource)
+    }
+
+    private fun Project.aQuery(): Node<PsiElement> {
         val query = mock<Node<PsiElement>>()
         val source = mock<PsiElement>()
         val containingFile = mock<PsiFile>()
+        val backingVirtualFile = mock<VirtualFile>()
 
-        whenever(source.project).thenReturn(project)
+        whenever(source.project).thenReturn(this)
         whenever(query.source).thenReturn(source)
         whenever(source.containingFile).thenReturn(containingFile)
+        whenever(containingFile.virtualFile).thenReturn(backingVirtualFile)
+        whenever(containingFile.project).thenReturn(this)
 
         return query
     }
 
-    private suspend fun Project.connectedTo(dataSource: LocalDataSource) {
-        val connectionViewModel by service<ConnectionStateViewModel>()
-        connectionViewModel.selectDataSource(dataSource)
+    private fun Project.connectedTo(
+        dataSource: LocalDataSource,
+    ) {
+        val uniqueId = dataSource.uniqueId
+
+        val dbDataSource = mock<DbDataSource>()
+        whenever(dbDataSource.uniqueId).thenReturn(uniqueId)
+        whenever(dbDataSource.localDataSource).thenReturn(dataSource)
+
+        val dbPsiFacade = mock<DbPsiFacade>()
+        whenever(dbPsiFacade.findDataSource(any())).thenReturn(dbDataSource)
+
+        withMockedService(dbPsiFacade)
+
+        val viewModel = mock<ConnectionStateViewModel>()
+        val connectionState = MutableStateFlow(
+            ConnectionState.default().withConnectionState(
+                SelectedConnectionState.Connected(dataSource)
+            )
+        )
+
+        whenever(viewModel.mutableConnectionState).thenReturn(connectionState)
+        whenever(viewModel.connectionState).thenReturn(connectionState)
+
+        withMockedService(viewModel)
     }
 }
