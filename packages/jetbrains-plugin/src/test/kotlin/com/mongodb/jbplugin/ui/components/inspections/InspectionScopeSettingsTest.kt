b1e9b107a0e90b8c90123ae54e71ce0739d26689
Kevin Mas Ruiz <kevin.mas@hey.com>
2025-04-04 18:52:16 +0200

    chore: new ui under test

--- packages/jetbrains-plugin/src/test/kotlin/com/mongodb/jbplugin/ui/components/inspections/InspectionScopeSettingsTest.kt
+++ packages/jetbrains-plugin/src/test/kotlin/com/mongodb/jbplugin/ui/components/inspections/InspectionScopeSettingsTest.kt
@@ -1,13 +1,19 @@
 package com.mongodb.jbplugin.ui.components.inspections
 
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.runComposeUiTest
 import com.mongodb.jbplugin.fixtures.setContentWithTheme
 import com.mongodb.jbplugin.inspections.analysisScope.AnalysisScope
 import com.mongodb.jbplugin.ui.viewModel.AnalysisStatus
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import kotlin.time.Duration.Companion.minutes
 
 @OptIn(ExperimentalTestApi::class)
 class InspectionScopeSettingsTest {
@@ -20,5 +26,74 @@ class InspectionScopeSettingsTest {
         onNodeWithTag("InspectionScopeComboBox")
             .assertExists()
             .assertTextEquals("Current File")
+    }
+
+    @Test
+    fun `should show the progress of an analysis when collecting files`() = runComposeUiTest {
+        setContentWithTheme {
+            _InspectionScopeSettings(AnalysisScope.default(), AnalysisStatus.CollectingFiles)
+        }
+
+        onNodeWithText("Collecting files to analyze.").assertExists()
+    }
+
+    @Test
+    fun `should show the progress of an analysis when analysing files`() = runComposeUiTest {
+        setContentWithTheme {
+            _InspectionScopeSettings(AnalysisScope.default(), AnalysisStatus.InProgress(setOf(""), setOf()))
+        }
+
+        onNodeWithText("Analyzed 0 of 1 files.").assertExists()
+    }
+
+    @Test
+    fun `should show when the analysis is done`() = runComposeUiTest {
+        setContentWithTheme {
+            _InspectionScopeSettings(AnalysisScope.default(), AnalysisStatus.Done(10, 5.minutes))
+        }
+
+        onNodeWithText("Processed 10 files in 5 minutes.").assertExists()
+    }
+
+    @Test
+    fun `should have a refresh button that when clicked triggers an analysis`() = runComposeUiTest {
+        var didRefresh = false
+        val callbacks = InspectionScopeSettingsCallbacks(
+            onRefreshAnalysis = { didRefresh = true }
+        )
+
+        setContentWithTheme {
+            CompositionLocalProvider(LocalInspectionScopeSettingsCallbacks provides callbacks) {
+                _InspectionScopeSettings(
+                    AnalysisScope.default(),
+                    AnalysisStatus.default()
+                )
+            }
+        }
+
+        onNodeWithText("Refresh").assertExists().performClick()
+        assertTrue(didRefresh)
+    }
+
+    @Test
+    fun `detects when an scope has changed in the scope dropdown`() = runComposeUiTest {
+        var selectedScope: AnalysisScope = AnalysisScope.CurrentFile
+        val callbacks = InspectionScopeSettingsCallbacks(
+            onScopeChange = { selectedScope = it }
+        )
+
+        setContentWithTheme {
+            CompositionLocalProvider(LocalInspectionScopeSettingsCallbacks provides callbacks) {
+                _InspectionScopeSettings(
+                    AnalysisScope.default(),
+                    AnalysisStatus.default()
+                )
+            }
+        }
+
+        onNodeWithTag("InspectionScopeComboBox").assertExists().performClick()
+        onNodeWithTag("InspectionScopeComboBox::Item::AllInsights").assertExists().performClick()
+
+        assertEquals(selectedScope, AnalysisScope.AllInsights)
     }
 }
