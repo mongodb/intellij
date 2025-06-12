8ae9deef784f5a6d3481be3b9c35c8ee54c1ee36
Kevin Mas Ruiz <kevin.mas@hey.com>
2025-06-12 14:37:06 +0200

    chore: colours for light theme (#245)
    * chore: colours for light theme
    
    * chore: colours for light theme
    
    chore: make theme reload when it changes in the config

--- packages/jetbrains-plugin/src/main/kotlin/com/mongodb/jbplugin/ui/components/utilities/ActionLink.kt
+++ packages/jetbrains-plugin/src/main/kotlin/com/mongodb/jbplugin/ui/components/utilities/ActionLink.kt
@@ -5,9 +5,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextOverflow
+import com.intellij.ui.JBColor
 import org.jetbrains.jewel.ui.component.Text
-
-private val LinkColor = Color(0x6B, 0x9B, 0xFA)
 
 @Composable
 fun ActionLink(
@@ -21,7 +20,12 @@ fun ActionLink(
         text,
         overflow = overflow,
         maxLines = maxLines,
-        color = LinkColor,
+        color = ActionLink.LinkColor,
         modifier = modifier.clickable { onClick() }
     )
+}
+
+object ActionLink {
+    val LinkColor: Color
+        get() = if (JBColor.isBright()) Color(0x2B, 0x5B, 0xAA) else Color(0x6B, 0x9B, 0xFA)
 }
